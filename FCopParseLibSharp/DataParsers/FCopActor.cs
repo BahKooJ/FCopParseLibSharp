d4bd1bfb9f28cedf63651ec117527087b69a8490
BahKooJ <71286169+BahKooJ@users.noreply.github.com>
2023-02-04 12:56:46 -0800

    Parsing of FCop scripting

---
+++ FCopParseLibSharp/DataParsers/FCopActor.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace FCopParser {
+
+    public class FCopActor {
+
+        const int idOffset = 8;
+        const int objectTypeOffset = 12;
+        const int yOffset = 16;
+        const int xOffset = 24;
+
+        public FCopActor(IFFDataFile rawFile) {
+
+        }
+
+
+    }
+
+}
--- FCopParseLibSharp/Main.cs
+++ FCopParseLibSharp/Main.cs
@@ -1,15 +1,46 @@
 ﻿using System.Collections;
 using FCopParser;
 
-var parser = new IFFParser(File.ReadAllBytes("C:/Program Files (x86)/Electronic Arts/Future Cop/missions/Un"));
+var parser = new IFFParser(File.ReadAllBytes("Mp"));
 
 
-var net = new FCopNavMesh(parser.parsedData.files.First(file => {
+var foo = new FCopFunction(parser.parsedData.files.First(file => {
 
-    return file.dataFourCC == "Cnet";
+    return file.dataFourCC == "Cfun";
 
 }));
 
+//foo.tEXTData[27].line1 = new List<byte> { 0 };
+//foo.tEXTData[27].line2.RemoveRange(60, 4);
+
+
+//var indexOfDataChange = 692;
+//var dataMin = 4;
+
+//foreach (var item in foo.tFUNData) {
+
+//    if (item.startingOffset > indexOfDataChange) {
+//        item.startingOffset -= dataMin;
+//    }
+
+//    if (item.endingOffset > indexOfDataChange) {
+//        item.endingOffset -= dataMin;
+//    }
+
+//}
+
+var item = foo.tEXTData[5];
+
+foreach (var i in Enumerable.Range(0, item.line1.Count)) {
+    item.line1[i] = 0;
+}
+
+foo.Compile();
+
+new FCopFunction(foo.rawFile);
+
 parser.Compile();
+
+File.WriteAllBytes("Mp MOD", parser.bytes);
 
 Console.WriteLine("bonk");
